220475b1fa28e9c58c8bca5e84978b9b88025e3f
andreeajitaru <jitaruxandreea@gmail.com>
2024-11-18 14:32:15 -0800

    added slider component

--- app/tabs/profile/newgoal.js
+++ app/tabs/profile/newgoal.js
@@ -1,8 +1,19 @@
 import { StyleSheet, View } from "react-native";
 import Theme from "@/assets/theme";
+import Slider from "@react-native-community/slider";
 
 export default function NewGoal() {
-  return <View style={styles.container}></View>;
+  return (
+    <View style={styles.container}>
+      <Slider
+        style={{ width: 200, height: 40 }}
+        minimumValue={0}
+        maximumValue={1}
+        minimumTrackTintColor="#FFFFFF"
+        maximumTrackTintColor="#000000"
+      />
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
