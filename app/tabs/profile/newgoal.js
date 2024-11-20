2e35501e6ef8b14646828ad7ce826c9565d54f07
andreeajitaru <jitaruxandreea@gmail.com>
2024-11-20 14:46:50 -0800

    no styling on new goal, just components

--- app/tabs/profile/newgoal.js
+++ app/tabs/profile/newgoal.js
@@ -1,10 +1,33 @@
-import { StyleSheet, View } from "react-native";
+import { StyleSheet, View, TextInput } from "react-native";
 import Theme from "@/assets/theme";
 import Slider from "@react-native-community/slider";
+import { useState } from "react";
+import { SelectList } from "react-native-dropdown-select-list";
 
 export default function NewGoal() {
+  const [textName, onChangeTextName] = useState("your goal here");
+  const data = [
+    { label: "Item 1", value: "1" },
+    { label: "Item 2", value: "2" },
+    { label: "Item 3", value: "3" },
+    { label: "Item 4", value: "4" },
+    { label: "Item 5", value: "5" },
+    { label: "Item 6", value: "6" },
+    { label: "Item 7", value: "7" },
+    { label: "Item 8", value: "8" },
+  ];
   return (
     <View style={styles.container}>
+      <TextInput
+        style={styles.goalNameInputText}
+        onChangeText={onChangeTextName}
+        value={textName}
+      />
+      <SelectList
+        setSelected={(val) => setSelected(val)}
+        data={data}
+        save="value"
+      />
       <Slider
         style={{ width: 200, height: 40 }}
         minimumValue={0}
